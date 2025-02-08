a9578700f96334846e2d8059036185ccf8a2bcb6
Pranav Goyanka <pgoyanka@gmail.com>
2025-02-08 12:37:00 -0500

    Fixed grammar in About

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,12 +4,14 @@ const About = () => {
       <h1 className="text-3xl font-bold text-center mt-4">about</h1>
       <p className="text-justify mt-2">
         I am a software developer with over 2 years of experience specializing
-        in Distributed Systems and backend engineering. I have recently
-        graduated from the Master of Science in Computer Science at Boston
-        University, where I am also conducted research on stream processing
-        engines to optimize resource utilization. In addition to my academic
-        focus, I served a teaching assistant and course designer for the
-        graduate Distributed Systems course.
+        in distributed systems and backend engineering. I recently graduated
+        with a Master of Science in Computer Science from Boston University,
+        where I also conducted research on stream processing engines to optimize
+        resource utilization on edge devices. In addition to my academic focus,
+        I served as a teaching assistant and course designer for the
+        graduate-level Distributed Systems course. I am also active in the
+        open-source community and have participated in and mentored for Google
+        Summer of Code.
       </p>
     </>
   );
